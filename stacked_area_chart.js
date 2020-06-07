57aedbdeb77e4a228479ebacb71f5fee80054f52
Jonne Kaunisto <jonneka@gmail.com>
2020-06-07 11:46:44 -0700

    added prof suggestions

--- stacked_area_chart.js
+++ stacked_area_chart.js
@@ -136,7 +136,7 @@ function createStackedChart(svg_container, index) {
             .attr("text-anchor", "end")
             .attr("x", 0)
             .attr("y", -20)
-            .text("percentage of days spend doing a specific activity")
+            .text("Percent time spent in various activities")
             .attr("text-anchor", "start")
 
         // Add Y axis
